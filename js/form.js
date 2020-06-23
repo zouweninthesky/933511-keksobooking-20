90ed339b78418760143fc00f13d6f3784803ab81
Zouwen <zouweninthesky@yandex.ru>
2020-06-23 22:09:53 +0300

    Добавляет проверку оставшихся полей формы

--- js/form.js
+++ js/form.js
@@ -3,9 +3,65 @@
 (function () {
   var mainPin = document.querySelector('.map__pin--main');
   var adForm = document.querySelector('.ad-form');
+  var adTitle = adForm.querySelector('#title');
   var adAddress = adForm.querySelector('#address');
+  var adType = adForm.querySelector('#type');
+  var adPrice = adForm.querySelector('#price');
+  var adTimeIn = adForm.querySelector('#timein');
+  var adTimeOut = adForm.querySelector('#timeout');
   var adRoomNumber = adForm.querySelector('#room_number');
   var adCapacity = adForm.querySelector('#capacity');
+
+  var minPriceValues = {
+    'bungalo': 0,
+    'flat': 1000,
+    'house': 5000,
+    'palace': 10000
+  };
+
+  // Получает координаты нижнего конца главной метки
+  var getCoordinates = function (start) {
+    var x = parseInt(mainPin.style.left.replace(/[^+\d]/g, ''), 10);
+    var y = parseInt(mainPin.style.top.replace(/[^+\d]/g, ''), 10);
+    x += window.util.PIN_WIDTH_HALF;
+    if (start) {
+      y += window.util.PIN_WIDTH_HALF;
+    } else {
+      y += window.util.PIN_HEIGHT;
+    }
+    adAddress.value = x + ', ' + y;
+  };
+
+  var checkTitleLength = function () {
+    if (adTitle.value.length < 30) {
+      adTitle.setCustomValidity('Минимум 30 символов!');
+      adTitle.reportValidity();
+    } else if (adTitle.value.length > 100) {
+      adTitle.setCustomValidity('Максимум 100 символов!');
+      adTitle.reportValidity();
+    } else {
+      adTitle.setCustomValidity('');
+    }
+  };
+
+  var checkType = function (option) {
+    adPrice.min = minPriceValues[option];
+    adPrice.placeholder = minPriceValues[option];
+  };
+
+  var checkPrice = function () {
+    if (adPrice.value > 1000000) {
+      adPrice.setCustomValidity('Максимальная цена - 1 миллион!');
+      adPrice.reportValidity();
+    } else {
+      adPrice.setCustomValidity('');
+      adPrice.reportValidity();
+    }
+  };
+
+  var changeTimeInput = function (first, second) {
+    second.value = first.value;
+  };
 
   // Проверяет соответствие количества комнат количеству гостей
   var checkRoomCapacityInput = function () {
@@ -27,6 +83,26 @@
     }
   };
 
+  adTitle.addEventListener('input', function () {
+    checkTitleLength();
+  });
+
+  adType.addEventListener('change', function () {
+    checkType(adType.value);
+  });
+
+  adPrice.addEventListener('input', function () {
+    checkPrice();
+  });
+
+  adTimeIn.addEventListener('change', function () {
+    changeTimeInput(adTimeIn, adTimeOut);
+  });
+
+  adTimeOut.addEventListener('change', function () {
+    changeTimeInput(adTimeOut, adTimeIn);
+  });
+
   adCapacity.addEventListener('change', function () {
     checkRoomCapacityInput();
   });
@@ -34,19 +110,6 @@
   adRoomNumber.addEventListener('change', function () {
     checkRoomCapacityInput();
   });
-
-  // Получает координаты нижнего конца главной метки
-  var getCoordinates = function (start) {
-    var x = parseInt(mainPin.style.left.replace(/[^+\d]/g, ''), 10);
-    var y = parseInt(mainPin.style.top.replace(/[^+\d]/g, ''), 10);
-    x += window.util.PIN_WIDTH_HALF;
-    if (start) {
-      y += window.util.PIN_WIDTH_HALF;
-    } else {
-      y += window.util.PIN_HEIGHT;
-    }
-    adAddress.value = x + ', ' + y;
-  };
 
   window.form = {
     getCoordinates: getCoordinates
